6abc51ea5e4407d1b259b99a66fd912c9ade57bd
usmanasghar78 <osmanasghar78@gmail.com>
2019-05-04 02:10:17 +0500

    Display Users Successfully

--- app/src/main/java/com/example/rabta/Adapter/UserAdapter.java
+++ app/src/main/java/com/example/rabta/Adapter/UserAdapter.java
@@ -1,4 +1,61 @@
 package com.example.rabta.Adapter;
 
-public class UserAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.rabta.Model.User;
+import com.example.rabta.R;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    private Context mContext;
+    private List<User> mUsers;
+
+
+public UserAdapter (Context mContext, List<User> mUsers){
+    this.mUsers=mUsers;
+    this.mContext=mContext;
+}
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int position) {
+        View view = LayoutInflater.from(mContext).inflate(  R.layout.user_item,parent,false);
+    return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+User user = mUsers.get(position);
+holder.username.setText(user.getUsername());
+if(user.getImageURL().equals("default")){
+    holder.profile_image.setImageResource(R.mipmap.ic_launcher);
+
+}else {
+    Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
+}
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+    public TextView username;
+    public ImageView profile_image;
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+        username=itemView.findViewById(R.id.username);
+        profile_image=itemView.findViewById(R.id.profile_image);
+    }
+}
 }
