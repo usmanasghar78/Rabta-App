c61414657e6f17efcd67332cb26cabba139ad106
unknown <rjaweria78@gmail.com>
2019-05-13 23:41:30 +0500

    online offline methods update

--- app/src/main/java/com/example/rabta/MainActivity.java
+++ app/src/main/java/com/example/rabta/MainActivity.java
@@ -30,6 +30,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -129,3 +130,24 @@ DatabaseReference reference;
     }
 }
 
+    private void status(String status){
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("status", status);
+
+        reference.updateChildren(hashMap);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        status("online");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        status("offline");
+    }
+}
